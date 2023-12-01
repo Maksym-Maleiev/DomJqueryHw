c987189f17e2c2b7d75a280ac7780c6bdcad8392
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-01 10:12:56 +0200

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -3,5 +3,7 @@
 var nameFriends = ["Harry", "Ron", "Hermione"];
 
 for (var i = 0; i < 3; i++){
-  
+  $("body").append("<p>My frind name is</p> " + nameFriends[0]);
+  $("body").append("<p>My frind name is</p> " + nameFriends[1]);
+  $("body").append("<p>My frind name is</p> " + nameFriends[2]);
 }
