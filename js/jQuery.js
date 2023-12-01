b8281ae3dec02b9d5ac1066315c60ba87ec22149
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-01 10:31:04 +0200

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -4,10 +4,8 @@ var nameFriends = ["Harry", "Ron", "Hermione"];
 
 for (var i = 0; i < nameFriends.length; i++){
   $("body").append("<p>My friend name is " + nameFriends[i] + "</p>");
+  $("p").hide(0);
+  $("p").fadeIn(1000);
 }
 
 $("h1").text("Meet my friends:");
-
-$(nameFriends[0]).hide(2000).fadeIn(2000);
-$(nameFriends[1]).hide(2000).fadeIn(2000);
-$(nameFriends[2]).hide(2000).fadeIn(2000);
