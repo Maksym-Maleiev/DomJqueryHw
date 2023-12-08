0c43ff5c40548b24596a22b41e2f34a137833ab4
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-08 14:41:40 +0200

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -4,11 +4,11 @@ var nameFriends = ["Harry", "Ron", "Hermione"];
 
 for (var i = 0; i < nameFriends.length; i++){
   $("body").append("<p>" + nameFriends[i] + "</p>");
-  $("p").hide();
-  $("p").fadeIn(2000);
 }
 
 $("h1").text("My friends:");
+$("p").hide();
+  $("p").fadeIn(2000);
 $("p").append(" smells");
 
 // 2: create a flashing header
