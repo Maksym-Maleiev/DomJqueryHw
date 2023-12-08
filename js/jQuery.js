6edc597a0bf6ab1f74bac95d6785abf75bcbe644
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-12-08 14:49:09 +0200

    Update jQuery.js

--- js/jQuery.js
+++ js/jQuery.js
@@ -11,10 +11,10 @@ $("p").hide().fadeIn(2000);;
 $("p").append(" smells");
 
 // 2: create a flashing header
-for (var j = 0; j < 5; j++){
+for (var j = 0; j < 1; j++){
   $("h1").fadeOut(1000).fadeIn(1000);
-  $("h1").fadeOut(1000).fadeIn(1000);
-  $("h1").fadeOut(1000).fadeIn(1000);
-  $("h1").fadeOut(1000).fadeIn(1000);
-  $("h1").fadeOut(1000).fadeIn(1000);
+  $("h1").fadeOut(2000).fadeIn(2000);
+  $("h1").fadeOut(3000).fadeIn(3000);
+  $("h1").fadeOut(4000).fadeIn(4000);
+  $("h1").fadeOut(5000).fadeIn(5000);
 }
